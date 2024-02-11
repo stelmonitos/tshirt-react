04c15d6c87754d40d87bc90193f66cfbe33d593b
Jakub Stelmasiak <stelmon3@gmail.com>
2024-02-11 22:10:58 +0100

    addCart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,12 +5,28 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 const Product = props => {
 
+  const addToCard = e => {
+    e.preventDefault();
+    console.log('Summary');
+    console.log('=======================');
+    console.log('Name:', props.title);
+    console.log('Price:', getPrice());
+    console.log('Size:', currentSize);
+    console.log('Colours:', currentColor);
+  }
+
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
 
   const prepareColorClassName = color => {
     return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
   };
+
+  const getPrice = () => {
+    const size = props.sizes.find(size => size.name === currentSize);
+    const additionalPrice = size ? size.additionalPrice : 0;
+    return props.basePrice + additionalPrice;
+  }
 
   return (
     <article className={styles.product}>
@@ -24,7 +40,7 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {props.basePrice}$</span>
+          <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
@@ -56,9 +72,11 @@ const Product = props => {
                       </li>))}
                     </ul>
                   </div>
+                  <form onClick={addToCard}>
                   <Button className={styles.button}>
-                    <span className="fa fa-shopping-cart" />
+                    <span  className="fa fa-shopping-cart" />
                   </Button>
+                  </form>
                 </form>
               </div>
     </article>
