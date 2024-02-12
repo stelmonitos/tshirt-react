085464f8ab852c87cc017467387421d923f62ee6
Jakub Stelmasiak <stelmon3@gmail.com>
2024-02-12 11:12:31 +0100

    add ProductImage

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,6 +3,8 @@ import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import ProductImage from '../ProductImage/ProductImage';
+
 const Product = props => {
 
   const addToCard = e => {
@@ -30,13 +32,7 @@ const Product = props => {
 
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt={`${props.title}`}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
-      </div>
-      
+      <ProductImage name={props.name} currentColor={currentColor}/>
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
